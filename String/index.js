504b8c48e244628a96259c908d9c884c7c796d87
Lucas Lima Fernandes <lucas.lfernandes@live.com>
2020-04-18 15:10:34 -0300

    Primeira commit, curso JS ebook Kindle

---
+++ String/index.js
@@ -0,0 +1,9 @@
+let str = 'Teste'
+console.log(str)
+console.log('Tamanho: ' + str.length)
+console.log('CAIXA ALTA: ' + str.toUpperCase())
+console.log('Caixa baixa: ' + str.toLowerCase())
+str = '1'
+console.log("de '1' para: " + parseInt(str))
+str = '1.5'
+console.log("de '1.5' para: " + parseFloat(str))
