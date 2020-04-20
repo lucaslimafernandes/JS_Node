0ef399ea2b234014ebecd0c24ff817a416497044
Lucas Lima Fernandes <lucas.lfernandes@live.com>
2020-04-19 21:58:17 -0300

    Switch Case add

---
+++ SwitchCase/index.js
@@ -0,0 +1,13 @@
+var letra = 'A'
+
+switch (letra) {
+
+  case 'A': console.log('Vogal A'); break
+  case 'E': console.log('Vogal E'); break
+  case 'I': console.log('Vogal I'); break
+  case 'O': console.log('Vogal O'); break
+  case 'U': console.log('Vogal U'); break
+
+  default: console.log('Não é uma vogal')
+
+}
